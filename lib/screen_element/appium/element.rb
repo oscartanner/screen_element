17d0c4cb0855ab4e20578b5ca510362ac8fa0013
Oscar Tanner <oscar.tanner@99taxis.com>
2017-01-30 13:42:08 -0200

    Added Appium World and Element

---
+++ lib/screen_element/appium/element.rb
@@ -0,0 +1,67 @@
+require_relative 'world'
+
+module Appium
+  class Element < AppiumWorld
+    def initialize(type, identificator)
+      if type == :desc
+        @type = :xpath
+        @identificator = "//*[@content-desc='#{identificator}']"
+      else
+        @type = type
+        @identificator = identificator
+      end
+    end
+
+    def visible!(opt = {})
+      timeout = opt.fetch(:timeout, 10)
+      begin
+        wait_true(timeout) { element.displayed? }
+      rescue => e
+        raise ElementNotFoundError,
+              "Element with #{@type}: '#{@identificator}' not found!\n#{e.message}"
+      end
+    end
+
+    def visible?(opt = {})
+      begin
+        visible!(opt)
+      rescue
+        return false
+      end
+      true
+    end
+
+    def touch(opt = {})
+      visible!(opt)
+      element.click
+    end
+
+    def drag(opt = {})
+      delta_x = opt.fetch(:delta_x, 0)
+      delta_y = opt.fetch(:delta_y, 0)
+      Appium::TouchAction.new.long_press(element: element)
+                         .move_to(x: delta_x, y: delta_y).release.perform
+    end
+
+    def swipe(opt = {})
+      delta_x = opt.fetch(:delta_x, 0)
+      delta_y = opt.fetch(:delta_y, 0)
+      location = element.location
+      # TODO: Remove global variable $driver
+      $driver.swipe(start_x: location[:x], start_y: location[:y],
+                    delta_x: delta_x, delta_y: delta_y)
+    end
+
+    # TODO: See if would be nice to implement these methods with method missing
+    # to avoid creating all these accessor methods for element attributes
+    def text
+      element.text
+    end
+
+    private
+
+    def element
+      find_element(@type, @identificator)
+    end
+  end
+end
