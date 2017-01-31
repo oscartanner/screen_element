08c8d35b3810dbae8365888c7d45f684d8f2cb67
Oscar Tanner <oscar.tanner@99taxis.com>
2017-01-31 14:31:30 -0200

    More tests for appium world class

--- spec/appium_env/world_test.rb
+++ spec/appium_env/world_test.rb
@@ -9,9 +9,21 @@ describe ScreenElement::AppiumEnv::World do
 
   describe 'Methods' do
     context 'Take Screenshot Method' do
+      exception_message = 'Screenshot path is null!! Please set option[:path].'
       it 'If path is null, an exception should be thrown' do
         expect { ScreenElement::AppiumEnv::World.take_screenshot }
-          .to raise_exception 'Screenshot path is null!! Please set option[:path].'
+          .to raise_exception exception_message
+      end
+
+      it 'Screenshot path can be set as an class variable' do
+        ScreenElement::AppiumEnv::World.screenshot_path = 'test'
+        expect { ScreenElement::AppiumEnv::World.take_screenshot }
+          .not_to raise_exception exception_message
+      end
+
+      it 'Screenshot path can be set as an optional parameter' do
+        expect { ScreenElement::AppiumEnv::World.take_screenshot(path: 'test') }
+          .not_to raise_exception exception_message
       end
     end
   end
