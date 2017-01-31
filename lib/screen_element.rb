3513bb17a9779db36aa818f3a220a9f98c08f319
Oscar Tanner <oscar.tanner@99taxis.com>
2017-01-31 17:22:45 -0200

    Added base screen and removed element module
    With base screen in the lib, element module became useless

--- lib/screen_element.rb
+++ lib/screen_element.rb
@@ -1,3 +1,3 @@
 require_relative "#{File.dirname(__FILE__)}/screen_element/appium_env/world"
 require_relative "#{File.dirname(__FILE__)}/screen_element/appium_env/element"
-require_relative "#{File.dirname(__FILE__)}/screen_element/appium_env/element_module"
+require_relative "#{File.dirname(__FILE__)}/screen_element/appium_env/base_screen"
---
+++ lib/screen_element/appium_env/base_screen.rb
@@ -0,0 +1,23 @@
+module ScreenElement
+  module AppiumEnv
+    class BaseScreen
+      class << self
+        def element(name, type = :pending, identificator = '')
+          define_method(name.to_s) { Element.new type, identificator }
+        end
+      end
+
+      def initialize(opt = {})
+        check_trait opt
+      end
+
+      def check_trait(opt)
+        raise "#{self.class} not found" unless trait.visible?(opt)
+      end
+
+      def message(message)
+        Element.new(:text, message)
+      end
+    end
+  end
+end
